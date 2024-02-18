4134b007c00d34c85029ecf4471a8fe3040fc806
YogLn <15508067205@163.com>
2024-02-18 15:06:54 +0800

    feat: debounce immediate

--- 13.防抖.js
+++ 13.防抖.js
@@ -1,11 +1,33 @@
-const debounce = (fn, delay = 300) => {
+// const debounce = (fn, delay = 300) => {
+//   let timer = null;
+//   return function (...args) {
+//     clearInterval(timer);
+//     const context = this;
+//     const args = arguments;
+//     timer = setTimeout(() => {
+//       fn.apply(context, apply);
+//     }, delay);
+//   };
+// };
+
+// 进阶，增加一个参数，控制立即执行
+const debounce = (fn, delay = 1000, immediate = false) => {
   let timer = null;
   return function (...args) {
-    clearInterval(timer);
-    const context = this;
-    const args = arguments;
-    timer = setTimeout(() => {
-      fn.apply(context, apply);
-    }, delay);
+    let that = this;
+    timer && clearTimeout(timer);
+    if (immediate) {
+      let callNow = !timer;
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+      if (callNow) {
+        fn.call(that, ...args);
+      }
+    } else {
+      timer = setTimeout(() => {
+        fn.call(that, ...args);
+      }, delay);
+    }
   };
 };
